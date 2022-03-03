7e0a6515ad8104b987a8ffa6ee5824976737803c
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-03-03 09:21:47 +0100

    del

--- exam-projects/wild-life/config/routes.js
+++
@@ -1,9 +0,0 @@
-const homeController = require('../controllers/home');
-const authController = require('../controllers/auth');
-const postController = require('../controllers/post');
-
-module.exports = (app) => {
-   app.use(homeController);
-   app.use(authController);
-   app.use(postController);
-}
